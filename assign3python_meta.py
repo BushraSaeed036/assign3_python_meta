732b1f200d8a494ace2952abbab64641336cf9f7
Bushra Saeed <bushrasaeed036@gmail.com>
2025-05-12 01:39:01 +0500

    Add files via upload

---
+++ assign3python_meta.py
@@ -0,0 +1,24 @@
+# Declare Variables
+fruit = "Banana"
+quantity = 6
+color_name = "Yellow"
+
+# Invalid Variable Examples
+#variable cant start with a number
+#6fruit = "Banana"
+#spaces are not allowed
+#color name = "Yellow"
+#dashes are  not allowed
+#color--name = "Yellow"
+
+# Snake Case Style
+my_name = "Bushra"
+my_age = 20
+my_city = "Faisalabad"
+
+print(fruit)
+print(quantity)
+print(color_name)
+print(my_name)
+print(my_age)
+print(my_city)
